c1aaa01fb02f5deaf9c176e7ba13fe35138b2221
Heet <heetnandu01@gmail.com>
2025-04-26 00:24:05 +0530

    made Songs play

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 console.log("Lets write javascript")
+    async function getSongs() {
+        let a = await fetch("http://127.0.0.1:3000/soundwave/songs/");
+        let response = await a.text();
+        let div = document.createElement("div");
+        div.innerHTML = response;
+        let as = div.getElementsByTagName("a");
+        let songs = []
 
-async function main() {
-    let a = await fetch("http://127.0.0.1:3000/soundwave/songs/")
-    let response = await a.text()
-    console.log(response)
-}
+        for (let index = 0; index < as.length; index++) {
+            const element = as[index];
+            if (element.href.endsWith(".mp3")) {
+                songs.push(element.href);
+            }
+        }
 
-main();
+        return songs
+    }
+
+    async function main() {
+        let songs = await getSongs();
+        console.log(songs)
+
+        var audio = new Audio(songs[0]);
+        audio.play();
+
+        audio.addEventListener("loadeddata",() =>{
+            let duration = audio.duration;
+            console.log(duration)
+        });
+    }
+main()
