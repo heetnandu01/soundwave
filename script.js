d83a6a8a90e9981aefdc71e2422bd3db797b87fc
Heet <heetnandu01@gmail.com>
2025-04-27 01:12:28 +0530

    fetched songs names properly

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-console.log("Lets write javascript")
-    async function getSongs() {
-        let a = await fetch("http://127.0.0.1:3000/soundwave/songs/");
-        let response = await a.text();
-        let div = document.createElement("div");
-        div.innerHTML = response;
-        let as = div.getElementsByTagName("a");
-        let songs = []
+console.log("Let's write JavaScript");
 
-        for (let index = 0; index < as.length; index++) {
-            const element = as[index];
-            if (element.href.endsWith(".mp3")) {
-                songs.push(element.href);
-            }
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/soundwave/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href); // Keep full href
         }
-
-        return songs
     }
 
-    async function main() {
-        let songs = await getSongs();
-        console.log(songs)
+    return songs;
+}
 
-        var audio = new Audio(songs[0]);
-        audio.play();
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
 
-        audio.addEventListener("loadeddata",() =>{
-            let duration = audio.duration;
-            console.log(duration)
-        });
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    
+    for (const song of songs) {
+        let songName = song.split("/").pop().replaceAll("%20", " ");
+        songUL.innerHTML = songUL.innerHTML + `<li>${songName}</li>`;
     }
-main()
+
+    var audio = new Audio(songs[0]);
+    // audio.play(); // (you had commented it out)
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    });
+}
+
+main();
